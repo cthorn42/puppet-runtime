a9618ed783a6b63cea88aee557030e0d51612f9c
Christopher Thorn <christopher.thorn@puppet.com>
2022-05-26 14:12:47 -0700

    (maint) Add cmake for ubuntu 2204
    The VM image does not have cmake installed by default causing failure during
    packaging of the puppet-agent.

--- configs/platforms/ubuntu-22.04-amd64.rb
+++ configs/platforms/ubuntu-22.04-amd64.rb
@@ -10,6 +10,7 @@ platform "ubuntu-22.04-amd64" do |plat|
     swig
     systemtap-sdt-dev
     zlib1g-dev
+    cmake
   )
   plat.provision_with "export DEBIAN_FRONTEND=noninteractive && apt-get update -qq && apt-get install -qy --no-install-recommends #{packages.join(' ')}"
   plat.provision_with "curl https://artifactory.delivery.puppetlabs.net/artifactory/api/gpg/key/public | apt-key add -"
